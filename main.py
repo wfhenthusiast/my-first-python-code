59ff72eb3a3c8a0f2c957b2d74919fbd6e7f3351
Daniel Ari Hutapea <hutapeadaniel36@gmail.com>
2023-02-24 18:11:02 +0700

    belajar sekuensial 1

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 """
-Ini adalah demo project pertama dengan python
+Semua sintaksis dasar bahasa pemrograman terdiri dari:
+1. Sekuensial   : langkah berurutan
+2. Percabangan  : langkah melompat jika kondisi terpenuhi
+3. Perulangan   : mengulang langkah yang sama berkali kali selama/sampai kondisi terpenuhi
+shortcut menyeleksi ctrl+shift+panah
+shortcut menggeser kursor secara cepat ctrl+panah
 """
-print("Hello World")
-print("Hello World")
-print("Ini Siapa")
+#sekuensial
+print('Mom said, "Pergi sana"')
+print('Anak menjawab, "Apaan"')
+print('Ibu menyuruh, "Belikan susu 1, kalau ada telur, belikan 5"')
