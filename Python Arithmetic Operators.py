1f5abf41c0b4fd7438d35c18813074fbcc72ba7c
Daniel Ari Hutapea <hutapeadaniel36@gmail.com>
2023-02-27 18:01:29 +0700

    Python Operators

---
+++ Python Arithmetic Operators.py
@@ -0,0 +1,31 @@
+#Python Arithmetic Operators
+#Additon
+x=10
+y=3
+hasil = x+y
+print(hasil)
+
+#Subtraction
+hasil = x-y
+print(hasil)
+
+#Multiplication
+hasil = x*y
+print(hasil)
+
+#Division
+hasil = x/y
+print(hasil)
+
+#Modulus
+hasil = x%y
+print(hasil)
+
+#Exponentiation
+#sama seperti x dipangkatkan sebanyak y
+hasil = x ** y
+print(hasil)
+
+#Floor Division
+hasil = x // y
+print(hasil)
