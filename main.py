42f1235540cb406d3ec0eb5c3bd23c7bd890e17b
Daniel Ari Hutapea <hutapeadaniel36@gmail.com>
2023-02-27 17:56:56 +0700

    operator aritmatika python

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ print('Si Anak menjawab, "Karena mereka mempunyai telur"')
 #Python Arithmetic Operators
 #Additon
 x=10
-y=5
+y=3
 hasil = x+y
 print(hasil)
 
@@ -27,4 +27,21 @@ print(hasil)
 
 #Multiplication
 hasil = x*y
+print(hasil)
+
+#Division
+hasil = x/y
+print(hasil)
+
+#Modulus
+hasil = x%y
+print(hasil)
+
+#Exponentiation
+#sama seperti x dipangkatkan sebanyak y
+hasil = x ** y
+print(hasil)
+
+#Floor Division
+hasil = x // y
 print(hasil)
