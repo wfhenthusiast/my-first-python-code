c2ae28d0d4fe24046088c83390f60523ee66a5a4
Daniel Ari Hutapea <hutapeadaniel36@gmail.com>
2023-03-01 12:16:48 +0700

    hello world

--- Python Arithmetic Operators.py
+++ Python Arithmetic Operators.py
@@ -43,3 +43,30 @@ print(daftar_siswa)
 #slicing index
 #tanda koma dalam list menandakan index atau nomor urut yang dimulai dari 0
 print(daftar_siswa[:3])
+
+#tipe data mapping (array asosiatif)
+daftar_siswa = {
+    "1": "luhut",
+    "2": "binsar",
+    "3": "panjaitan",
+    "4": "riki",
+    "5": "faisal"
+}
+
+for angka, siswa in daftar_siswa.items() :
+    print(f"angka: {angka} nama: {siswa}")
+print(daftar_siswa['3'])
+
+
+nama_kota = {
+    "1": "medan",
+    "2": "surabaya",
+    "3": "jogja",
+    "4": "maluku",
+    "5": "sidrap"
+}
+
+for nomor, kota in nama_kota.items() :
+    print(f"nomor: {nomor} kota: {kota}")
+
+from ctypes import c_double
