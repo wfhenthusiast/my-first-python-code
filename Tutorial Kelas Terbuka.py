6ba21d56ea9778a8d43a0ed7db4d187f157bc5d9
Daniel Ari Hutapea <hutapeadaniel36@gmail.com>
2023-03-03 10:57:07 +0700

    belajar variabel python

--- Tutorial Kelas Terbuka.py
+++ Tutorial Kelas Terbuka.py
@@ -10,3 +10,21 @@ ini adalah comment multiline
 """
 a=11
 print(a)
+"""
+kita bisa mengcompile python ke yang namanya bytecode dengan cara buka terminal dan 
+tuliskan python -m py_compile Main.py (nama file)
+"""
+
+#variabel, adalah tempat menyimpan data
+
+#assignment nilai
+#di python tidak perlu deklarasi variabel
+a=10
+x=20
+lebar=30
+print("lebarnya adalah", lebar)
+
+#penamaan
+nilai_y = 20
+#penamaan dua variabel tidak boleh menggunakan spasi tapi boleh menggunakan _
+#variabel tidak boleh diawali dengan angka
