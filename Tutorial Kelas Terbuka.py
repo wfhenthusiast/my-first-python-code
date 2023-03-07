6f039c13a5046099b17098d1f2b80ff0355e745b
Daniel Ari Hutapea <hutapeadaniel36@gmail.com>
2023-03-07 12:47:58 +0700

    belajar casting tipe data python

--- Tutorial Kelas Terbuka.py
+++ Tutorial Kelas Terbuka.py
@@ -90,12 +90,26 @@ print("- bertipe ", type(data_c_double))
 #casting tipe data = merubah dari satu tipe data ke tipe lain
 #tipe data: int, float, str, bool
 
-data_int = 2
+#mengubah dari data integer
+print("---INTEGER---")
+data_int = 2;
 print("data : ", data_int, ", type =", type(data_int))
 
 data_float = float(data_int)
 data_str = str(data_int)
 data_bool = bool(data_int) #akan false jika nilai int = 0, akan true kalau nilai int > 0
 print("data = ", data_float, ", type=", type(data_float))
+print("data = ", data_str, ", type=", type(data_str))
+print("data = ", data_bool, ", type=", type(data_bool))
+
+#mengubah dari data float
+print("---FLOAT---")
+data_float = 2.2;
+print("data : ", data_float, ", type =", type(data_float))
+
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float) #akan false jika nilai int = 0, akan true kalau nilai int > 0
+print("data = ", data_int, ", type=", type(data_int))
 print("data = ", data_str, ", type=", type(data_str))
 print("data = ", data_bool, ", type=", type(data_bool))
